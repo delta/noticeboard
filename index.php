ab6921bdf5253a2e7488295236182e1c9a53dcf4
Boopathi Rajaa <boopathi@live.com>
2011-08-16 23:33:14 +0530

    templates

--- index.php
+++ index.php
@@ -5,5 +5,37 @@ define("NOTICEBOARD", "!@#$%^&*()");
 include("config.inc.php");
 include("includes/functions.lib.php");
 
+$page = "";
+if(isset($_GET["page"]))
+	$page = $_GET["page"];
+else
+	$page = "home";
 
+switch($page):
+
+case "innovate":
+	include("./innovate.php");
+	break;
+
+case "solve":
+	include("./solve.php");
+	break;
+
+case "complaint":
+	include("./complaint.php");
+	break;
+
+case "home":
+default:
+?>
+
+<!doctype html>
+<html>
+
+</html>
+
+<?php
+
+
+endswitch;
 ?>
